dfeb2922439723ed1906de688cbf061509b8988d
Tahir Y. Dibirov <tahiryd@code.local>
2023-01-12 18:18:56 +0400

    INIT

--- src/components/AddQuestion.jsx
+++ src/components/AddQuestion.jsx
@@ -5,7 +5,7 @@ export const AddQuestion = () => {
   return (
     <div className='add-overlay'>
         <div className="add-modal">
-            <h1>Answer a question?</h1>
+            <h1>Ask a question?</h1>
             <input placeholder='Your question' type="text" />
             <button className='add-ask-btn'>Ask</button>
         </div>
